da089064ec45558b1d9f7d2420f16937925129fd
Ian Kidd <ian@kidd.farm>
2025-01-31 14:38:25 -0700

    Updated MqttDataViewer to show last sensor readings with headings

--- mqtt-data-viewer/src/app/MqttDataViewer.tsx
+++ mqtt-data-viewer/src/app/MqttDataViewer.tsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react';
 import mqtt from 'mqtt';
 
 const MqttDataViewer: React.FC = () => {
-  const [thcData, setThcData] = useState<string[]>([]);
-  const [bmeData, setBmeData] = useState<string[]>([]);
+  const [thcData, setThcData] = useState('');
+  const [bmeData, setBmeData] = useState('');
 
   useEffect(() => {
     const client = mqtt.connect('ws://localhost:9001/mqtt');
@@ -18,9 +18,9 @@ const MqttDataViewer: React.FC = () => {
     client.on('message', (topic, message) => {
       const data = message.toString();
       if (topic.includes('THC-S')) {
-        setThcData((prevData) => [...prevData, data]);
+        setThcData(data);
       } else if (topic.includes('BME280')) {
-        setBmeData((prevData) => [...prevData, data]);
+        setBmeData(data);
       }
     });
 
@@ -33,17 +33,11 @@ const MqttDataViewer: React.FC = () => {
     <div>
       <h1>MQTT Data Viewer</h1>
       <h2>THC-S Sensor Data</h2>
-      <ul>
-        {thcData.map((data, index) => (
-          <li key={index}>{data}</li>
-        ))}
-      </ul>
+      <h2>Last THC-S Sensor Data</h2>
+      <p>{thcData}</p>
       <h2>BME280 Sensor Data</h2>
-      <ul>
-        {bmeData.map((data, index) => (
-          <li key={index}>{data}</li>
-        ))}
-      </ul>
+      <h2>Last BME280 Sensor Data</h2>
+      <p>{bmeData}</p>
     </div>
   );
 };
